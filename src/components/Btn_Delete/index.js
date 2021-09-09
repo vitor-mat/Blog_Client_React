61d1dde0fa6247583b10f87d4bb68cb1a6b61c09
vitor-mat <vitormateusalves123@gmail.com>
2021-09-09 03:28:48 -0300

    funcao de deletar posts funcionando

--- src/components/Btn_Delete/index.js
+++ src/components/Btn_Delete/index.js
@@ -1,10 +1,19 @@
 import React from "react";
 
+import { api } from "../../services/api";
+
 import { Button } from "./style";
 
-export const BtnDelete = () => {
+export const BtnDelete = (props) => {
+
+    const handleDeletePosts = () => {
+        api.delete(`/delete/${props.id}`)
+            .then(res => alert(props.id))
+            .catch(err => alert("Não foi possivel deletar o post: "+err))
+    }
+
     return(
-        <Button>
+        <Button onClick={handleDeletePosts}>
             Deletar
         </Button>
     )
