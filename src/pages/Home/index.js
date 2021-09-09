d66e450606116661d142e406544295052fc623e7
vitor-mat <vitormateusalves123@gmail.com>
2021-09-09 09:12:12 -0300

    mostrando postes na pagina inicial

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
+import { api } from "../../services/api";
+
 import { Container, Header, Main } from "./style";
 
-//import { Card } from "../../components/card/index";
+import { Card } from "../../components/card/index";
 
 export const Home = () => {
+
+    let [myPosts, setMyPosts] = useState([])
+
+    api.get("/posts")
+        .then(res => setMyPosts(myPosts = res.data))
+        .catch(err => alert("Error! Erro ao se conectar com o banco de dados: "+err))
+
     return(
         <Container>
             <Header>
@@ -16,7 +25,9 @@ export const Home = () => {
                 </div>
             </Header>
             <Main>
-
+                {myPosts.map(value => {
+                    return <Card key={value.id} title={value.title} description={value.description} content={value.content} />
+                })}
             </Main>
         </Container>
     )
